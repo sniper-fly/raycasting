dae9df92acaf2aa1cf0c43e03852bc22f76cf203
rnakai <rnakai@student.42tokyo.jp>
2020-10-15 12:22:11 +0900

    hooks imp

---
+++ raycasting_c/hooks.c
@@ -0,0 +1,23 @@
+#include "minilibx-linux/mlx.h"
+
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+int		close(int keycode, t_vars *vars)
+{
+	mlx_destroy_window(vars->mlx, vars->win);
+}
+
+int		main(void)
+{
+	t_vars	vars;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "hello world");
+	mlx_key_hook(vars.win, close, &vars);
+	mlx_loop(vars.mlx);
+
+	return (0);
+}
