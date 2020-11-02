f7d811539347bd42ff0113a7b3299e77dad18157
rnakai <rnakai@student.42tokyo.jp>
2020-11-02 13:19:42 +0900

    draw_lineやdraw_rectangleを拝借しmapを描画。my_mlx_pixel_putも改善、簡略化

--- raycasting_c/udemy/raycasting.c
+++ raycasting_c/udemy/raycasting.c
@@ -16,6 +16,8 @@
 # define PI 3.14159265
 # define TWO_PI 6.28318530
 
+# define TO_COORD(X, Y) ((int)floor(Y) * WIDTH + (int)floor(X))
+
 #define FOV_ANGLE (60 * (PI / 180))
 #define NUM_RAYS WIDTH * HEIGHT
 
@@ -76,6 +78,9 @@ typedef struct
 
 
 int			is_game_running = FALSE;
+int			g_player_x = 20;
+int			g_player_y = 20;
+
 char		*window = NULL;
 char		*renderer = NULL;
 
@@ -91,10 +96,90 @@ int		initialize_window(t_game *game)
 
 void	my_mlx_pixel_put(t_game *game, int x, int y, int color)
 {
-	int		*dst;
+	// int		*dst;
 
-	dst = game->img.data + (y * game->img.size_l + x * game->img.bpp / (sizeof(int) * 8));
-	*dst = color;
+	game->img.data[y * WIDTH + x] = color;
+	// dst = game->img.data + (y * game->img.size_l + x * game->img.bpp / (sizeof(int) * 8));
+	// *dst = color;
+}
+
+void	draw_line(t_game *game, double x1, double y1, double x2, double y2)
+{
+	double	deltaX;
+	double	deltaY;
+	double	step;
+
+	deltaX = x2 - x1;
+	deltaY = y2 - y1;
+	step = (fabs(deltaX) > fabs(deltaY)) ? fabs(deltaX) : fabs(deltaY);
+	deltaX /= step;
+	deltaY /= step;
+	while (fabs(x2 - x1) > 0.01 || fabs(y2 - y1) > 0.01)
+	{
+		game->img.data[TO_COORD(x1, y1)] = 0xb3b3b3;
+		x1 += deltaX;
+		y1 += deltaY;
+	}
+}
+
+void 	draw_lines(t_game *game)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (i < COLS)
+	{
+		draw_line(game, i * TILE_SIZE, 0, i * TILE_SIZE, HEIGHT);
+		i++;
+	}
+	draw_line(game, COLS * TILE_SIZE - 1, 0, COLS * TILE_SIZE - 1, HEIGHT);
+	j = 0;
+	while (j < ROWS)
+	{
+		draw_line(game, 0, j * TILE_SIZE, WIDTH, j * TILE_SIZE);
+		j++;
+	}
+	draw_line(game, 0, ROWS * TILE_SIZE - 1, WIDTH, ROWS * TILE_SIZE - 1);
+}
+
+void	draw_rectangle(t_game *game, int x, int y)
+{
+	int i;
+	int j;
+
+	x *= TILE_SIZE;
+	y *= TILE_SIZE;
+	i = 0;
+	while (i < TILE_SIZE)
+	{
+		j = 0;
+		while (j < TILE_SIZE)
+		{
+			game->img.data[(y  + i) * WIDTH + x + j] = 0xFFFFFF;
+			j++;
+		}
+		i++;
+	}
+}
+
+void	draw_rectangles(t_game *game)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (i < ROWS)
+	{
+		j = 0;
+		while (j < COLS)
+		{
+			if (g_map[i][j] == 1)
+				draw_rectangle(game, j, i);
+			j++;
+		}
+		i++;
+	}
 }
 
 void	render_rectangle(t_game *game, t_rect_info *rect)
@@ -109,38 +194,51 @@ void	render_rectangle(t_game *game, t_rect_info *rect)
 	while (rect->x < x_end)
 	{
 		rect->y = y_tmp;
-		while (rect->y <= y_end)
+		while (rect->y < y_end)
 		{
 			my_mlx_pixel_put(game, rect->x, rect->y, rect->color);
-			rect->y++;
+			rect->y += 1;
 		}
-		rect->x++;
+		rect->x += 1;
 	}
 }
 
 void	render_map(t_game *game)
 {
-	t_rect_info	rect;
-	int			i;
-	int			j;
+	draw_rectangles(game);
+	draw_lines(game);
 
-	i = 0;
-	rect.width = TILE_SIZE;
-	rect.height = TILE_SIZE;
-	while (i < ROWS)
-	{
-		j = 0;
-		while (j < COLS)
-		{
-			rect.x = j * TILE_SIZE;
-			rect.y = i * TILE_SIZE;
-			rect.color = g_map[i][j] != 0 ? 0xff : 0;
-			render_rectangle(game, &rect);
-			j++;
-		}
-		i++;
-	}
+	// t_rect_info	rect;
+	// int			i;
+	// int			j;
+	// i = 0;
+	// rect.width = TILE_SIZE;
+	// rect.height = TILE_SIZE;
+	// while (i < ROWS)
+	// {
+	// 	j = 0;
+	// 	while (j < COLS)
+	// 	{
+	// 		rect.x = j * TILE_SIZE;
+	// 		rect.y = i * TILE_SIZE;
+	// 		rect.color = g_map[i][j] != 0 ? 0xff : 0;
+	// 		render_rectangle(game, &rect);
+	// 		j++;
+	// 	}
+	// 	i++;
+	// }
+}
 
+void	render_player(t_game *game)
+{
+	t_rect_info rect;
+	
+	rect.x = g_player_x;
+	rect.y = g_player_y;
+	rect.width = 10;
+	rect.height = 10;
+	rect.color = 0x00ff00;
+	render_rectangle(game, &rect);
 }
 
 void	setup(void)
@@ -167,7 +265,7 @@ void	render(t_game *game)
 {
 	render_map(game);
 	// render_rays();
-	// render_player();
+	render_player(game);
 	mlx_put_image_to_window(game->mlx, game->win, game->img.img, 0, 0);
 }
 
